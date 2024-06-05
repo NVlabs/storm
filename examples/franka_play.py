cbce6d78cdbdd97e3a02775796de8e1af1f89a95
Senthurbavan <senthurbavankiruba@gmail.com>
2024-06-05 10:35:14 -0400

    franka play completed

--- examples/franka_play.py
+++ examples/franka_play.py
@@ -84,28 +84,65 @@ def mpc_robot_interactive(args):
     device = torch.device('cuda', 0)
     tensor_args = {'device': device, 'dtype': torch.float32}
 
+    recorded_data = torch.load('record_data')
+
     mpc_control = ReacherTask(task_file, robot_file, world_file, tensor_args)
 
-    x_des = np.array([-0.3, 0.3, 0.2, -2.0, 0.0, 2.4, 0.0,
-                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    x_des = recorded_data['x_des']
     mpc_control.update_params(goal_state=x_des)
 
     sim_dt = mpc_control.exp_params['control_dt']
-    w_T_r = 0 # load from recorded data
-    recorded_length = 0 # the length of the recorded data
+    assert sim_dt == recorded_data['sim_dt']
 
-    g_pos = np.ravel(mpc_control.controller.rollout_fn.goal_ee_pos.cpu().numpy())
-    g_q = np.ravel(mpc_control.controller.rollout_fn.goal_ee_quat.cpu().numpy())
+    w_T_r_data = recorded_data['w_T_r']
+    w_T_r_p = gymapi.Vec3(w_T_r_data[0], w_T_r_data[1], w_T_r_data[2])
+    w_T_r_r = gymapi.Quat(w_T_r_data[3], w_T_r_data[4], w_T_r_data[5], w_T_r_data[6])
+    w_T_r = gymapi.Transform(p=w_T_r_p, r=w_T_r_r) # load from recorded data
+    recorded_length = recorded_data['recorded_length'] # the length of the recorded data
 
+    goal_pose_lst = recorded_data['goal_pose']
+    t_step_lst = recorded_data['t_step']
+    current_robot_state_lst = recorded_data['current_robot_state']
+    command_lst = recorded_data['command']
+
+    assert recorded_length == len(goal_pose_lst) == len(t_step_lst) == len(current_robot_state_lst) == len(command_lst)
+
+    print(f'Recorded length: {recorded_length}')
+    # for i in range(recorded_length):
+        # print(f'\ntime step: {t_step_lst[i]}')
+        # print(f'goal pose: {goal_pose_lst[i]}')
+        # print(f'curr state')
+        # [print(k, current_robot_state_lst[i][k]) for k in current_robot_state_lst[i].keys()]
+        # print(f"command: {command_lst[i]['position']}")
+
+    print('Calculating the loop...')
     for i in range(recorded_length):
-        goal_pose = None # load from recorded data
+        goal_pose = goal_pose_lst[i]
         if goal_pose is not None:
             g_pos, g_q = goal_pose
             mpc_control.update_params(goal_ee_pos=g_pos,
                                       goal_ee_quat=g_q)
 
-        t_step = 0  # load from recorded data
-        current_robot_state = None # load from recorded data
+        t_step = t_step_lst[i]
+        current_robot_state = current_robot_state_lst[i]
 
         command = mpc_control.get_command(t_step, current_robot_state, control_dt=sim_dt, WAIT=True)
         q_des = copy.deepcopy(command['position'])
+        q_des_target = copy.deepcopy((command_lst[i])['position'])
+
+        # print(f'i:{i} recorded and computed q_des\n\t{q_des_target}\n\t{q_des}')
+        cmd_error = (q_des_target - q_des)
+        cmd_error_ratio = abs(cmd_error / q_des_target)*100
+        if sum(cmd_error_ratio>5):
+            print(f'step: {i} error:', end='\t[')
+            [print(f' {e:.2f} ', end='') for e in cmd_error_ratio]
+            print(']')
+    print(f'Recorded length: {recorded_length}')
+    print('End')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='pass args')
+    parser.add_argument('--robot', type=str, default='franka', help='Robot to spawn')
+    args = parser.parse_args()
+
+    mpc_robot_interactive(args)
